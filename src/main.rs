a89eb0e0061b3214fe23fc29260e8a5d9438e08f
VITALII VOROBIOV <noruffle@gmail.com>
2024-08-02 15:26:13 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
-use actix_web::{get, web, App, HttpServer};
-
-// This struct represents state
-struct AppState {
-    app_name: String,
-}
-
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name; // <- get app_name
-    format!("Hello {app_name}!") // <- response with app_name
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Actix Web"),
-            }))
-            .service(index)
+            .service(hello)
+            .service(echo)
+            .service(manual_hello)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/api")]
+async fn echo() -> impl Responder {
+    HttpResponse::Ok().body("API")
+}
+
+#[get("/hey")]
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
